9a3541cd106e2ff2a0872ad96a079e28faac2b09
ShlomiTofahi <shlomitofahi@gmail.com>
2021-03-10 15:57:11 +0200

    update contact and about pages

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 import { Row, Col, Container } from 'reactstrap';
+import peopleCommunity20Filled from '@iconify-icons/fluent/people-community-20-filled';
+
+import { Icon, InlineIcon } from '@iconify/react';
+import cutIcon from '@iconify-icons/whh/cut';
+import combIcon from '@iconify-icons/si-glyph/comb';
+import shopIcon from '@iconify-icons/entypo/shop';
+import petsIcon from '@iconify-icons/dashicons/pets';
 
 export default class About extends Component {
 
@@ -9,7 +16,7 @@ export default class About extends Component {
             webkitBorderRadius: '15px',
             mozBorderRadius: '15px',
             borderRadius: '15px',
-            marginTop: '30px',
+            marginTop: '10%',
             padding: '30px',
 
             webkitBoxShadow: '0 0 5px 0.1px #C7C7C7',
@@ -18,7 +25,7 @@ export default class About extends Component {
     };
     imgStyle = () => {
         return {
-            border: '1px solid rgb(230, 230, 230)',
+            // border: '1px solid rgb(230, 230, 230)',
             width: '90%',
             display: 'inline',
             webkitBorderRadius: '35px',
@@ -28,31 +35,82 @@ export default class About extends Component {
     };
     render() {
         return (
-            <Container className='mb-5'>
-                <div align="right" style={this.aboutStyle()}>
-                    <fieldset>
-                        <legend><h1 class='display-4' style={{ color: '#7c6f5a7a' }}>אז למה לכם בעצם, ומה אנחנו מציעים?</h1></legend>
-                        <Row>
-                            <Col xs="12" sm="12" md="6" className='pt-4' >
+            <div class='about-fullpage' align='right'>
+                <Container className='mt-4 mb-4'>
+                    <h1 class="brand"><span><strong>GDog</strong></span><Icon icon={petsIcon} /> | About Us</h1>
+                    <div class="wrapper animated bounceInLeft">
+                        <div class="company-info about-company-info text-center">
+                            <div style={iconStyle}>
+                                <Icon icon={peopleCommunity20Filled} width="80" />
+                                <span class='lead d-block'>קהילה תומכת</span>
+                            </div>
+                            <div class="haircut-iconn-container" style={iconStyle}>
+                                <Icon icon={cutIcon} width="80"  />
+                                <div class="overlay">
+                                    <Icon icon={combIcon} width="80"  />
+                                </div>
+                                <span class='lead d-block'>תספורת איכותית</span>
+                            </div>
+                            <div style={iconStyle}>
+                                <Icon icon={shopIcon} width="80" />
+                                <span class='lead d-block'>מתאים לכל כיס</span>
+                            </div>
+                            <div style={iconStyle}>
+                                <img style={petfoodStyle} src='/images/pet-food.png' alt='' />
+                                <span class='lead d-block'>שקי אוכל ברמה גבוה</span>
+                            </div>
+                            <div style={iconStyle}>
+                                <img style={petfoodStyle} src='/images/delivery.png' alt='' />
+                                <span class='lead d-block'>עובדים במתכונת משלוחים</span>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            {/* <h3 class='mb-3'>אז למה לכם בעצם, ומה אנחנו מציעים?</h3> */}
+                            <div align="right" style={this.aboutStyle()}>
+                                <fieldset>
+                                    <legend><h1 class='display-4' style={{ color: '#4d7448', opacity: '0.4' }}>אז למה לכם בעצם, ומה אנחנו מציעים?</h1></legend>
+                                    <Row>
 
-                                <img style={this.imgStyle()} src='/images/about.jpg' alt='' />
-                            </Col>
-                            <Col xs="12" sm="12" md="6" className='pt-4' >
+                                        <p class='lead' style={aboutTextStyle}>
 
-                                <p class='lead'>
+                                            הדף נוצר לאפשר לכל אדם שאוהב ומגדל כלב או חתול  לקבל את כל המידע, העדכונים והשירותים. מחירים שווים לכל כיס!
+                                            משפחה המגדלת כלב או חתול  זקוקה לא פעם לשירותים שונים הקשורים לבעלי חיים  ולמידע מהימן ואיכותי, שיכול לסייע לה בתהליך הגידול.  ברגע שעשיתם את הבחירה להיכנס לאתר הצטרפתם לקבוצה גדולה יותר של אנשים שיש להם מכנה משותף, אנחנו פה על מנת לספק לכם כל מה שאתם צריכים באהבה רבה🧡
+                                            הדף לאוהבי בעלי חיים מאפשר לקוראיו להיחשף למגוון תכנים איכותיים  לצד עצות פורום וטיפים נגישים, שאפשר ליישם.
+                                            יש לשים לב שהאתר אינו לוקח אחריות! הטיפים והעצות הניתנים בפורום לא מחליפים התייעצות מומחים, בכל עניין רצוי וכדאי לפנות גם לוטרינר/מאלף מוסמך.
+                                        </p>
 
-                                    הדף נוצר לאפשר לכל אדם שאוהב ומגדל כלב או חתול  לקבל את כל המידע, העדכונים והשירותים. מחירים שווים לכל כיס!
-                                    משפחה המגדלת כלב או חתול  זקוקה לא פעם לשירותים שונים הקשורים לבעלי חיים  ולמידע מהימן ואיכותי, שיכול לסייע לה בתהליך הגידול.  ברגע שעשיתם את הבחירה להיכנס לאתר הצטרפתם לקבוצה גדולה יותר של אנשים שיש להם מכנה משותף, אנחנו פה על מנת לספק לכם כל מה שאתם צריכים באהבה רבה🧡
-                                    הדף לאוהבי בעלי חיים מאפשר לקוראיו להיחשף למגוון תכנים איכותיים  לצד עצות פורום וטיפים נגישים, שאפשר ליישם.
-                                    יש לשים לב שהאתר אינו לוקח אחריות! הטיפים והעצות הניתנים בפורום לא מחליפים התייעצות מומחים, בכל עניין רצוי וכדאי לפנות גם לוטרינר/מאלף מוסמך
-                                </p>
-                            </Col>
+                                    </Row>
 
-                        </Row>
+                                </fieldset>
+                            </div>
 
-                    </fieldset>
-                </div>
-            </Container>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+
+
         )
     }
+}
+const iconStyle = {
+    position: 'relative',
+    // letf: '50%',
+    // right: '35%',
+    opacity: '0.8',
+    marginBottom: '25px'
+}
+const petfoodStyle = {
+    position: 'relative',
+    // letf: '50%',
+    // right: '35%',
+    width: '30%',
+    transform: 'scaleX(-1)',
+}
+const aboutTextStyle = {
+	webkitColumnCount:'2',
+	mozColumCount:'2',
+	columnCount:'2',
+    columnGap: '40px'
+
 }
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import ReactWhatsapp from 'react-whatsapp';
 
 import { getFilterItems } from '../actions/itemActions';
 
@@ -68,7 +69,7 @@ class Footer extends Component {
                         <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
                             <h5 class="text-uppercase">כלבים</h5>
 
-                            <ul class="list-unstyled mb-0 ml-5 pt-1">
+                            <ul class="list-unstyled mb-0  pt-1">
                                 <li class='mb-1'>
                                     <Link to="/products" onClick={this.dogItems} class="text-dark" >שקי אוכל</Link>
                                 </li>
@@ -86,7 +87,7 @@ class Footer extends Component {
                         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                             <h5 class="text-uppercase">חתולים</h5>
 
-                            <ul class="list-unstyled  mb-0 ml-5 pt-1">
+                            <ul class="list-unstyled  mb-0 pt-1">
                                 <li class='mb-1'>
                                     <Link to="/products" onClick={this.catItems} class="text-dark">שקי אוכל</Link>
                                 </li>
@@ -116,10 +117,14 @@ class Footer extends Component {
                                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
                                     </svg>
                                     <span class='pr-1'>topdogs5551@gmail.com</span>
-
                                 </a>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+                                    </svg>
+                                    <ReactWhatsapp number="972-050-213-0027" message="?מה שלומך" element='a' class='contact-btn' href="#"> הודעת וואטסאפ</ReactWhatsapp>
+                                </div>
                                 <br />
-                                <br /><br />
 
                             עקבו אחרינו
                             <br />
@@ -144,7 +149,7 @@ class Footer extends Component {
                 {/* <!-- Copyright --> */}
                 <div class="text-center p-1" style={{ backgroundColor: '#fafafa' }}>
                     {/* © 2020 Copyright: */}
-                    <a class="text-dark" href="#"><small>Copyright © 2021 GDog - All Rights Reserved</small></a>
+                    <a class="text-dark" href="https://www.linkedin.com/in/shlomi-tofahi-57316417b/" target="_blank"><small>Copyright © 2021 Shlomi Tofahi - All Rights Reserved</small></a>
                 </div>
                 {/* <!-- Copyright --> */}
             </footer>
